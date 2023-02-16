2c1dc13f1b05b9825e89ee85803f6bd5b901ed3b
JENNA_DUNFORD <Jenna.Dunford@bbd.co.za>
2023-02-16 17:13:41 +0200

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,19 +1,19 @@
 function displayEachPage(pageNumber) {
   switch (pageNumber) {
     case 0:
-      window.location.href = "Pages/homePage.html";
+      window.location.href = "/Pages/homePage.html";
       break;
     case 1:
-      window.location.href = "Pages/aboutPage.html";
+      window.location.href = "/Pages/aboutPage.html";
       break;
     case 2:
-      window.location.href = "Pages/experiencePage.html";
+      window.location.href = "/Pages/experiencePage.html";
       break;
     case 3:
-      window.location.href = "Pages/softwareProjects.html";
+      window.location.href = "/Pages/softwareProjects.html";
       break;
     case 4:
-      window.location.href = "Pages/artwork.html";
+      window.location.href = "/Pages/artwork.html";
       break;
   }
 }
